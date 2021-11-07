85c5e4a6fe372545edc5b5d0b87567f09b1ba0ff
Fırat Kocabay <12109835+firatkocabay@users.noreply.github.com>
2021-11-07 22:02:08 +0300

    added get director with an id

--- routes/director.js
+++ routes/director.js
@@ -1,15 +1,17 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 const Director = require('../models/Director');
 
-// router.get('/', (req, res) => {
-//   const allDirectorsPromise = Director.find({});
-//   allDirectorsPromise.then((directors) => {
-//     res.json(directors);
-//   }).catch((err) => {
-//     res.json(err);
-//   });
-// });
+router.post('/', (req, res) => {
+  const director = new Director(req.body);
+  const createdDirectorPromise = director.save();
+  createdDirectorPromise.then((data) => {
+    res.json({status: 200, message: 'Created new director.'});
+  }).catch((err) => {
+    res.json(err);
+  });
+});
 
 router.get('/', (req, res) => {
   const searchDirectorPromise = Director.aggregate([
@@ -56,11 +58,51 @@ router.get('/', (req, res) => {
   });
 });
 
-router.post('/', (req, res) => {
-  const director = new Director(req.body);
-  const createdDirectorPromise = director.save();
-  createdDirectorPromise.then((data) => {
-    res.json({status: 200, message: 'Created new director.'});
+router.get('/:directorId', (req, res) => {
+  const searchDirectorPromise = Director.aggregate([
+    {
+      $match: {
+        '_id': mongoose.Types.ObjectId(req.params.directorId)
+      }
+    },
+    {
+      $lookup: {
+        from: 'movies',
+        localField: '_id',
+        foreignField: 'director_id',
+        as: 'movies'
+      }
+    },
+    {
+      $unwind : {
+        path: '$movies',
+        preserveNullAndEmptyArrays: true
+      }
+    },
+    {
+      $group: {
+        _id: {
+          _id : '$_id',
+          name: '$name',
+          surname: '$surname',
+          bio: '$bio'
+        },
+        movies: {
+          $push: '$movies'
+        }
+      }
+    },
+    {
+      $project: {
+        _id : '$_id._id',
+        name: '$_id.name',
+        surname: '$_id.surname',
+        movies: '$movies'
+      }
+    }
+  ]);
+  searchDirectorPromise.then((data) => {
+    res.json(data);
   }).catch((err) => {
     res.json(err);
   });
