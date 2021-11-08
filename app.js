763bd0f1399e6ddbfe0850cdb7ee38345a73c0b2
Fırat Kocabay <12109835+firatkocabay@users.noreply.github.com>
2021-11-08 22:24:10 +0300

    added jwt for authenticate

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ const app = express();
 // DB Connection
 const db = require('./helper/db.js')();
 
+// Config
+const config = require('./config');
+app.set('api_secret_key', config.api_secret_key);
+
+// Middleware 
+const verifyToken = require('./middleware/verify-token');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -24,6 +31,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api', verifyToken);
 app.use('/api/movies', movieRouter);
 app.use('/api/directors', directorRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,64 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/register', (req, res, next) => {
+  const { username, password } = req.body;
+  console.log(password);
+  bcrypt.hash(password, 10, (err, hash) => {
+    const user = new User({
+      username,
+      password: hash
+    });
+    const createUserPromise = user.save();
+    createUserPromise.then((data) => {
+      res.json(data);
+    }).catch((err) => {
+      res.json(err);
+    });
+  });
+});
+
+router.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  User.findOne({
+    username
+  }, (err, user) => {
+    if (err)
+      throw err;
+    if (!user) {
+      res.json({
+        status: false,
+        message: 'Authentication failed, user not found!'
+      });
+    } else {
+      bcrypt.compare(password, user.password).then((result) => {
+        if (!result) {
+          res.json({
+            status: false,
+            message: 'Authentication failed, wrong password!'
+          });
+        } else {
+          const payload = {
+            username
+          };
+          const token = jwt.sign(payload, req.app.get('api_secret_key'), {
+            expiresIn: 600 // 10 saat
+          });
+          res.json({
+            status: true,
+            token
+          })
+        }
+      })
+    }
+  });
 });
 
 module.exports = router;
